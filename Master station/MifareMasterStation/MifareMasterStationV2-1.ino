421d12223d72be37ed8789566dceced1b6d1658c
Vit <halny@rambler.ru>
2018-10-10 16:19:23 +0300

    1.0.2

--- Master station/MifareMasterStation/MifareMasterStationV2-1.ino
+++ Master station/MifareMasterStation/MifareMasterStationV2-1.ino
@@ -13,12 +13,13 @@ const byte vers = 103; //version of software
 bool rf24_online = false;
 #ifdef HW_YU
 
+
 // Структура получаемых и передаваемых данных
-struct strSendDataNRF_my
+struct strSendDataNRF
 { 
 byte sendDataNRF[15];
 };
-strSendDataNRF_my sendNRF;
+strSendDataNRF sendNRF;
 
 
 
@@ -118,7 +119,7 @@ uint8_t dataBuffer[dataSize];
 void setup() {
 // --------------- NRF24l01 
  pinMode(VCC_NRF, INPUT);
- my_rf24_online = false;
+ rf24_online = false;
 // --------------- NRF24l01 ---
   
   for (byte i = 0; i < 6; i++) {
@@ -137,8 +138,8 @@ void setup() {
 void loop() {
   
 // --------------- NRF24l01 
- if (digitalRead(VCC_NRF) and !my_rf24_online) { setupNRF();  }
- if (!digitalRead(VCC_NRF)) { my_rf24_online = false;}
+ if (digitalRead(VCC_NRF) and !rf24_online) { setupNRF();  }
+ if (!digitalRead(VCC_NRF)) { rf24_online = false;}
 // --------------- NRF24l01 ---
   
   static uint32_t lastReadCardTime = 0;
@@ -407,7 +408,7 @@ void findFunc() {
 void writeMasterTime() {
 
 // --------------- NRF24l01
-if(my_rf24_online){
+if(rf24_online){
   
 sendNRF.sendDataNRF[pageInit_0] = 0;
 sendNRF.sendDataNRF[pageInit_1] = 250;
@@ -477,7 +478,7 @@ SendNRF();
 void  writeMasterNum(){
 
 // --------------- NRF24l01
-if(my_rf24_online){
+if(rf24_online){
   
 sendNRF.sendDataNRF[pageInit_0] = 0;
 sendNRF.sendDataNRF[pageInit_1] = 251;
@@ -538,7 +539,7 @@ SendNRF();
 void writeMasterPass() {
 
 // --------------- NRF24l01
-if(my_rf24_online){
+if(rf24_online){
   
 sendNRF.sendDataNRF[pageInit_0] = 0;
 sendNRF.sendDataNRF[pageInit_1] = 254;
@@ -940,7 +941,7 @@ void writeMasterSleep() {
 
   
 // --------------- NRF24l01
-if(my_rf24_online){
+if(rf24_online){
   
 sendNRF.sendDataNRF[pageInit_0] = 0;
 sendNRF.sendDataNRF[pageInit_1] = 252;
@@ -1013,7 +1014,7 @@ void getVersion() {
 
   clearBuffer();
 
-  addData(versi, function);
+  addData(vers, function);
   sendData(function, dataCount);
   packetCount = 0;
 }
@@ -1035,7 +1036,7 @@ void clearBuffer() {
 
 void setupNRF(){
  beep(50, 2); delay(200); beep(50, 1); delay(300); beep(50, 3); 
- my_rf24_online = true;
+ rf24_online = true;
 
     SPI.begin();
     delay(500);
